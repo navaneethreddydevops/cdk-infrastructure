4c4a0a4e7d5e1555c320dcf78e4525359a6c208e
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2024-08-30 23:37:58 -0500

    Adding code for backend api

--- lambdas/index.py
+++ lambdas/index.py
@@ -6,23 +6,23 @@ import uuid
 import botocore
 import imghdr
 
-s3 = boto3.client('s3')
-dynamodb = boto3.client('dynamodb')
+s3 = boto3.client("s3")
+dynamodb = boto3.client("dynamodb")
 
 
 def upload_metadata(key, userid):
-    table = os.environ['table']
-    bucket = os.environ['bucket']
-    reference = {'Bucket': {'S': bucket}, 'Key': {'S': key}}
+    table = os.environ["table"]
+    bucket = os.environ["bucket"]
+    reference = {"Bucket": {"S": bucket}, "Key": {"S": key}}
     response = dynamodb.put_item(
         TableName=table,
-        Item={"userid": {
-            'S': userid}, "photo_reference": {'M': reference}})
+        Item={"userid": {"S": userid}, "photo_reference": {"M": reference}},
+    )
     print(response)
 
 
 def upload_image(image_id, img, userid):
-    bucket = os.environ['bucket']
+    bucket = os.environ["bucket"]
     extension = imghdr.what(None, h=img)
     key = f"{image_id}.{extension}"
     try:
@@ -39,26 +39,26 @@ def handler(event, context):
     # Generate random image id
     image_id = str(uuid.uuid4())
 
-    data = json.loads(event['body'])
-    userid = data['userid']
-    img = base64.b64decode(data['photo'])
+    data = json.loads(event["body"])
+    userid = data["userid"]
+    img = base64.b64decode(data["photo"])
 
     if upload_image(image_id, img, userid):
         return {
-            'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Headers': '*',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
             },
-            'body': json.dumps('Success!')
+            "body": json.dumps("Success!"),
         }
     return {
-        'statusCode': 500,
-        'headers': {
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        "statusCode": 500,
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
         },
-        'body': json.dumps('Request Failed!')
+        "body": json.dumps("Request Failed!"),
     }
